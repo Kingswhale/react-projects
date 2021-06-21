a258fe8eec5ed571eb6c0ce370fb04dc54f365e8
Afeez Aderemi <hardeyreemyarfyc@gmail.com>
2021-06-21 05:52:32 +0100

    update

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from 'react'
+import './index.css'
 
-const List = () => {
+const List = ({ people, setPeople }) => {
+  const handleRemove = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+            <button
+              type='button'
+              onClick={() => handleRemove(id)}
+              className='btn'
+            >
+              Remove
+            </button>
+          </article>
+        )
+      })}
     </>
-  );
-};
+  )
+}
 
-export default List;
+export default List
